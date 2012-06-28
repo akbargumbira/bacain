a3c85e03e5740b5a4687a26fee38ce16f016a2c1
mukhammadifanto@gmail.com <mukhammadifanto@gmail.com@0615212f-1089-4c4f-ee4a-bbe4344e242f>
2012-06-28 06:23:51 +0000

    CompactControl.xaml and ComfortControl.xaml

---
+++ bacaIn/CompactControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace bacaIn
+{
+	public partial class CompactControl : UserControl
+	{
+		public CompactControl()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
